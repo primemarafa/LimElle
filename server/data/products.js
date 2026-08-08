189722fdd1a9a64e7f5c1eea722511ad09d5d0fb
Moustapha MARAFA <100478179+primemarafa@users.noreply.github.com>
2026-08-08 12:43:30 +0000

    feat: add API product seed data

---
+++ server/data/products.js
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: "robe-001",
+    name: "Robe élégante",
+    price: 35000,
+    weight: 0.8,
+    availability: "disponible",
+  },
+  {
+    id: "chaussure-001",
+    name: "Chaussures femme",
+    price: 30000,
+    weight: 1.2,
+    availability: "a_verifier",
+  },
+];
