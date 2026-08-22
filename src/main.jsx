dedd6859a0ae2b6ad172fbdb514223df956b75ce
Moustapha MARAFA <100478179+primemarafa@users.noreply.github.com>
2026-08-22 20:55:46 +0000

    Refactor main.jsx to simplify imports and rendering
    Updated import statements and removed ErrorBoundary.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import ErrorBoundary from "./components/ErrorBoundary";
-import "./styles.css";
+import './style.css' 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
